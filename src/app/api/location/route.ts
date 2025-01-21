334e976fe1ffb8e511a325d3aae6807dd49c25b3
Diogo Costa <diogo@okrasolar.com>
2025-01-21 11:21:17 -0300

    lint fix urghh

--- src/app/api/location/route.ts
+++ src/app/api/location/route.ts
@@ -1,6 +1,4 @@
-import { NextRequest } from "next/server"
-
-export async function GET(req: NextRequest) {
+export async function GET() {
     try {
         // Step 1: Fetch user's IP using ipify.org
         console.log("Fetching user IP...")
